fcd813feeb94a48e91bbfa42afaeb1e48869d5b5
almalkias <almalkias@icloud.com>
2025-01-06 16:16:51 +0300

    new commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ export default function Footer() {
                         <Link to="/" className="pt-3">
                             <img alt="" src={logo} className="w-[120px]" />
                         </Link>
-                        <div className="mt-4 text-white text-center sm:text-center">
+                        <div className="mt-4 text-center sm:text-center">
                             <p>الهاتف</p>
                             <p>+966 555 360 417</p>
                             <p>البريد الإلكتروني</p>
@@ -24,30 +24,30 @@ export default function Footer() {
                     </div>
 
                     {/* Social Media Links Section */}
-                    <div className="flex space-x-6 mt-4 sm:mt-0 text-white">
+                    <div className="flex gap-3 text-xl mt-4 sm:mt-0">
                         <a href="#" className="hover:text-blue-500 transition duration-300">
-                            <i className="fab fa-linkedin"></i> {/* Font Awesome icon for Facebook */}
+                            <i className="fab fa-linkedin"></i>
                         </a>
                         <a href="#" className="hover:text-blue-500 transition duration-300">
-                            <i className="fab fa-twitter"></i> {/* Font Awesome icon for Twitter */}
+                            <i className="fab fa-twitter"></i>
                         </a>
                         <a href="#" className="hover:text-pink-500 transition duration-300">
-                            <i className="fab fa-instagram"></i> {/* Font Awesome icon for Instagram */}
+                            <i className="fab fa-instagram"></i>
                         </a>
                         <a href="#" className="hover:text-red-500 transition duration-300">
-                            <i className="fab fa-youtube"></i> {/* Font Awesome icon for YouTube */}
+                            <i className="fab fa-youtube"></i>
                         </a>
                     </div>
                 </div>
 
                 {/* Additional Links Section (Optional) */}
                 <div className="my-6 pt-4 text-center">
-                    <p className="text-white text-sm">
+                    <p className="text-sm">
                         &copy; {new Date().getFullYear()} جميع الحقوق محفوظة
                     </p>
                 </div>
                 <hr />
-                <p className='text-white text-right mt-5' style={{direction:'rtl'}}>
+                <p className='text-right mt-5' style={{direction:'rtl'}}>
                     إن شركة حلول ثمر للتصميم هي شركة ذات مسؤولية محدودة مسجلة بالمملكة العربية السعودية وتعمل وفق أنظمتها وتهدف إلى تقديم حلول رقمية في النشاطات الإدارية والمالية الخاصة بالصناديق العائلية ولا تقدم أي مشورة أو توجيه في الأمور الخاصة بالصناديق. كما أن البنية النهائية لخدمات المنصة والتطبيق ليست بالضرورة ستكون متوفرة بالنموذج الاولي الذي نعمل على بنائه.
                 </p>
             </div>
